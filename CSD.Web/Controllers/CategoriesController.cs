249f96bb0a9c31801745d44f65a584a06e58fc76
Asik1985 <assylbekezhanuly@yahoo.com>
2023-03-31 21:02:04 +0600

    Delete confirm

--- CSD.Web/Controllers/CategoriesController.cs
+++ CSD.Web/Controllers/CategoriesController.cs
@@ -83,6 +83,30 @@ namespace CSD.Web.Controllers
             ViewBag.Categories = new SelectList(_db.Categories.Where(x => x.ParentId == 0), "Id", "Name");
             return View(category);
         }
+        // GET: Categories/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/DeleteConfirmed/5
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
 
     }
 }
