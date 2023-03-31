5937da6d300af92baf1f185d3bd87a792b313eb6
Asik1985 <assylbekezhanuly@yahoo.com>
2023-03-31 19:46:06 +0600

    Migration and Models

---
+++ CSD.Web/Data/CSDContext.cs
@@ -0,0 +1,15 @@
+﻿using CSD.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSD.Web.Data;
+
+public class CSDContext : DbContext
+{
+    public CSDContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Post> Posts { get; set; }
+}
+
