c800373ed169690c27f66022fa19ee6059099fa6
Asik1985 <assylbekezhanuly@yahoo.com>
2023-03-31 19:50:35 +0600

    CategoryController

--- CSD.Web/Controllers/CategoriesController.cs
+++ CSD.Web/Controllers/CategoriesController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSD.Web.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CSD.Web.Controllers
 {
     public class CategoriesController : Controller
     {
+        private readonly CSDContext _db;
+
+        public CategoriesController(CSDContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
